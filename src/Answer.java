42a3cb1f596885976dcce756bdce55c4b46c7e22
Wam <samuelwolff450@gmail.com>
2017-12-04 08:23:43 -0500

    fixed classes and added to Answer class

--- src/Answer.java
+++ src/Answer.java
@@ -1,12 +1,28 @@
 /**  * Created by student on 12/4/17.  */
 public class Answer{
-    City[] cits;
+    City[] cits= new City[20];
 
     public Answer(City[] cits) {
         this.cits = cits;
     }
 
+    public Answer() {
+    }
+
     public City[] getCits() {
         return cits;
+    }
+    public void create(City[] masterList){
+        City[]copy= masterList;
+        for (int i = 0; i < cits.length; i++) {
+            int ran= (int)(Math.random()*copy.length);
+            if(copy[ran]!=null) {
+                cits[i]= copy[ran];
+                copy[ran]=null;
+            }
+            else {
+                i--;
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ import javax.swing.*;
  * Created by student on 12/4/17.
  */
 public class Main extends JPanel{
+ static   Answer[] list= new Answer[20];
+    public static void start(){
+        TestCities test= new TestCities();
+        for (int i = 0; i < list.length; i++) {
+            list[i].create(test.getCities());
+        }
+    }
+
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Traveling Salesperson, HI WAM!");
