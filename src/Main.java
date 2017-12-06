09f6670a52781ad151a390208a842917b671a885
andreweggleston <andrew_eggleston@student.wayland.k12.ma.us>
2017-12-06 10:16:18 -0500

    main broken

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,7 @@ public class Main extends JPanel{
         }
     }
 
-
     public static void main(String[] args) {
-        JFrame window = new JFrame("Traveling Salesperson, HI WAM!");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setBounds(0, 0, 1000, 800); //(x, y, w, h)
-        Main panel = new Main();
-        panel.setFocusable(true);
-        panel.grabFocus();
-        window.add(panel);
-        window.setVisible(true);
-        System.out.println("bwekfast");
 
     }
 }
